d9ec879a62455bf0407a6d33d7474bdb1b61a530
Ivan Spirandelli <ivan@spirandelli.de>
2024-09-26 13:47:18 -0700

    restructuring persistence_calls

--- src/modules/Energies/src/persistence_computations.jl
+++ src/modules/Energies/src/persistence_computations.jl
@@ -1,34 +1,33 @@
-py"""
-import oineus as oin
-import numpy as np
-import torch
-import diode
-
-def get_interface_diagram(points, n_atoms_per_mol):
-    points = np.asarray(points)
-    simplices = diode.fill_alpha_shapes(points)
-    fil = oin.Filtration_double([oin.Simplex_double(s[0], s[1]) for s in simplices])
-
-    def is_multi(sigma):
-        has_a = has_b = False
-        for v in sigma.vertices:
-            if v < n_atoms_per_mol:
-                has_a = True
-            else:
-                has_b = True
-        return has_a and has_b
-
-    fil = fil.subfiltration(is_multi)
-    # second argument: True for cohomology, False for homology (incorrect for subfiltration)
-    dcmp = oin.Decomposition(fil, True)
-    params = oin.ReductionParams()
-    params.clearing_opt = False
-    dcmp.reduce(params)
-    dgm = dcmp.diagram(fil, include_inf_points=False)
-    return dgm
-"""
-
 function get_interface_diagram(points, n_atoms_per_mol)
+    py"""
+    import oineus as oin
+    import numpy as np
+    import torch
+    import diode
+
+    def get_interface_diagram(points, n_atoms_per_mol):
+        points = np.asarray(points)
+        simplices = diode.fill_alpha_shapes(points)
+        fil = oin.Filtration_double([oin.Simplex_double(s[0], s[1]) for s in simplices])
+
+        def is_multi(sigma):
+            has_a = has_b = False
+            for v in sigma.vertices:
+                if v < n_atoms_per_mol:
+                    has_a = True
+                else:
+                    has_b = True
+            return has_a and has_b
+
+        fil = fil.subfiltration(is_multi)
+        # second argument: True for cohomology, False for homology (incorrect for subfiltration)
+        dcmp = oin.Decomposition(fil, True)
+        params = oin.ReductionParams()
+        params.clearing_opt = False
+        dcmp.reduce(params)
+        dgm = dcmp.diagram(fil, include_inf_points=False)
+        return dgm
+    """
     py"get_interface_diagram"(points, n_atoms_per_mol)
 end
 
