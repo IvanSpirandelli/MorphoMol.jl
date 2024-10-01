94c920a53ffaabc3aced4edab45d7adc38bfafa3
Ivan Spirandelli <ivan@spirandelli.de>
2024-10-01 11:37:04 -0700

    renaming

--- src/modules/Utilities/src/realization.jl
+++ src/modules/Utilities/src/realization.jl
@@ -1,8 +1,8 @@
-function get_flat_realization(x, template_mol)
+function get_flat_realization(x, template_centers)
     n_mol = length(x) ÷ 6
-    [(hvcat((n_mol), [exp(Rotations.RotationVecGenerator(x[i:i+2]...)) * template_mol .+ x[i+3:i+5] for i in 1:6:length(x)]...)...)...]
+    [(hvcat((n_mol), [exp(Rotations.RotationVecGenerator(x[i:i+2]...)) * template_centers .+ x[i+3:i+5] for i in 1:6:length(x)]...)...)...]
 end
 
-function get_matrix_realization(x, template_mol)
-    [exp(Rotations.RotationVecGenerator(x[i:i+2]...)) * template_mol .+ x[i+3:i+5] for i in 1:6:length(x)]
+function get_matrix_realization(x, template_centers)
+    [exp(Rotations.RotationVecGenerator(x[i:i+2]...)) * template_centers .+ x[i+3:i+5] for i in 1:6:length(x)]
 end
