5bb56a04d2882c7f8373b21dba93499b9382d4a1
Ivan Spirandelli <ivan@spirandelli.de>
2024-10-16 13:53:37 -0700

    updating interface computations

--- src/modules/Energies/src/Energies.jl
+++ src/modules/Energies/src/Energies.jl
@@ -2,7 +2,7 @@ module Energies
     export solvation_free_energy_with_overlap_penalty
     export get_geometric_measures, get_geometric_measures_with_derivatives
     export get_geometric_measures_and_overlap_value, get_geometric_measures_and_overlap_value_with_derivatives
-    export get_interface_diagram, get_total_persistence, get_persistence
+    export get_interface_diagram_and_filtration, get_total_persistence, get_persistence
 
     using PyCall
     using StaticArrays
