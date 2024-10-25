5d46a5676fb7743fe6115a1eb5dbcd7b37718299
Ivan Spirandelli <ivan@spirandelli.de>
2024-10-24 17:42:09 -0700

    new interface computations

--- src/modules/Energies/src/Energies.jl
+++ src/modules/Energies/src/Energies.jl
@@ -2,13 +2,14 @@ module Energies
     export solvation_free_energy_with_overlap_penalty
     export get_geometric_measures, get_geometric_measures_with_derivatives
     export get_geometric_measures_and_overlap_value, get_geometric_measures_and_overlap_value_with_derivatives
-    export get_total_persistence_summed, get_total_persistence, get_divided_persistence, get_divided_persistence_summed
-    export get_interface_diagram, get_interface_diagram_and_filtration
+    export get_total_persistence_summed, get_total_persistence, get_death_by_birth_persistence, get_death_by_birth_persistence_summed
+    export get_interface_with_persistence
     export get_persistence_diagram
 
     using PyCall
     using StaticArrays
     using Rotations
+    using GeometryBasics
 
     include("morphometric_approach/ball_union_measures.jl")
     include("morphometric_approach/solvation_free_energy.jl")
--- src/modules/Energies/src/persistence_computations.jl
+++ src/modules/Energies/src/persistence_computations.jl
@@ -27,88 +27,153 @@ function get_total_persistence(dgm, weight::Float64 = 1.0)
     weight * sum((dgm[:,2] - dgm[:,1]))
 end
 
-function get_divided_persistence(dgm, weight::Float64 = 1.0)
+function get_death_by_birth_persistence(dgm, weight::Float64 = 1.0)
     weight * sum([dgm[i,2] / dgm[i,1] for i in 1:size(dgm)[1]])
 end
 
-function get_divided_persistence_summed(dim_dgms::Vector{Matrix{Float64}}, weights::Vector{Float64} = [-0.1, -0.1])
-    sum([get_divided_persistence(dgm, weight) for (dgm, weight) in zip(dim_dgms, weights)])
+function get_death_by_birth_persistence_summed(dim_dgms::Vector{Matrix{Float64}}, weights::Vector{Float64} = [-0.1, -0.1])
+    sum([get_death_by_birth_persistence(dgm, weight) for (dgm, weight) in zip(dim_dgms, weights)])
 end
 
-function get_interface_diagram_and_filtration(points, n_atoms_per_mol)
+function get_multichromatic_tetrahedra(points, n_atoms_per_mol)
     py"""
-    import oineus as oin
     import numpy as np
-    import torch
     import diode
 
-    def get_interface_diagram_and_filtration(points, n_atoms_per_mol):
-        points = np.asarray(points)
-        simplices = diode.fill_alpha_shapes(points)
-        fil = oin.Filtration_double([oin.Simplex_double(s[0], s[1]) for s in simplices])
-
+    def get_multichromatic_tetrahedra(points, n_atoms_per_mol):        
         def is_multi(sigma):
-            return len(set(v // n_atoms_per_mol for v in sigma.vertices)) >= 2
-        fil = fil.subfiltration(is_multi)
-
-        def recalculate_filtration_value(cell):
-            parts = [v // n_atoms_per_mol for v in cell.vertices]
-            n_parts = len(set(parts))
-            p_agg = [np.array([0.0, 0.0, 0.0]) for _ in range(n_parts)]
-            weights = [0 for _ in range(n_parts)]
-            for i, p in enumerate(parts):
-                p_agg[p] += points[cell.vertices[i]]
-                weights[p] += 1
-            bcs = [p_agg[i] / weights[i] for i in range(n_parts)]
-            filtration_value = sum([np.linalg.norm(bcs[i] - bcs[j]) for i in range(n_parts) for j in range(i + 1, n_parts)]) / n_parts
-            # convert to native Python float from numpy
-            return float(filtration_value)
-
-        new_values = [recalculate_filtration_value(cell) for cell in fil]
-        fil.set_values(new_values)
-
-        dcmp = oin.Decomposition(fil, True)
-        params = oin.ReductionParams()
-        params.clearing_opt = False
-        dcmp.reduce(params)
-        dgm = dcmp.diagram(fil, include_inf_points=False)
-        return dgm, fil
+            return len(set(v // n_atoms_per_mol for v in sigma)) >= 2
+        points = np.asarray(points)
+        tetrahedra = [vs for (vs,fs) in diode.fill_alpha_shapes(points) if len(vs) == 4 and is_multi(vs)]
+        return tetrahedra
     """
-    py"get_interface_diagram_and_filtration"(points, n_atoms_per_mol)
+    py"get_multichromatic_tetrahedra"(points, n_atoms_per_mol)
 end
 
-function get_interface_diagram(points, n_atoms_per_mol)
+function get_barycentric_subdivision_and_filtration(points, mc_tets)
+    barycenters = Vector{Point3f}([])
+    filtration = Vector{Tuple{Vector{Int}, Float64}}([])
+    total_vertices = 0
+    for vs in eachrow(mc_tets)
+        part_one = [v+1 for v in vs if div(v, 1206)==0]
+        part_two = [v+1 for v in vs if div(v, 1206)==1]
+        if length(part_one) == length(part_two) 
+            #Get Simplices and Values for even split
+            x,y = part_one[1], part_one[2]
+            u,v = part_two[1], part_two[2]
+            barycenters = [barycenters; [
+                get_barycenter(points, [x,y,u,v]),  #1
+                get_barycenter(points, [x,v]),      #2
+                get_barycenter(points, [x,y,v]),    #3
+                get_barycenter(points, [y,v]),      #4
+                get_barycenter(points, [y,u,v]),    #5
+                get_barycenter(points, [y,u]),      #6
+                get_barycenter(points, [x,y,u]),    #7
+                get_barycenter(points, [x,u]),      #8
+                get_barycenter(points, [x,u,v]),    #9
+            ]]
+            vertices = [
+                ([1] .+ total_vertices, euclidean(get_barycenter(points, [x,y]), get_barycenter(points, [u,v])) / 2.0), 
+                ([2] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [v])) / 2.0),
+                ([3] .+ total_vertices, euclidean(get_barycenter(points, [x,y]), get_barycenter(points, [v])) / 2.0),
+                ([4] .+ total_vertices, euclidean(get_barycenter(points, [y]), get_barycenter(points, [v])) / 2.0),
+                ([5] .+ total_vertices, euclidean(get_barycenter(points, [y]), get_barycenter(points, [u, v])) / 2.0),
+                ([6] .+ total_vertices, euclidean(get_barycenter(points, [y]), get_barycenter(points, [u])) / 2.0),
+                ([7] .+ total_vertices, euclidean(get_barycenter(points, [x,y]), get_barycenter(points, [u])) / 2.0),
+                ([8] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [u])) / 2.0),
+                ([9] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [v,u])) / 2.0),
+            ]
+            edges = [
+                ([1,2] .+ total_vertices, minimum([vertices[1][2], vertices[2][2]])),
+                ([1,3] .+ total_vertices, minimum([vertices[1][2], vertices[3][2]])),
+                ([1,4] .+ total_vertices, minimum([vertices[1][2], vertices[4][2]])),
+                ([1,5] .+ total_vertices, minimum([vertices[1][2], vertices[5][2]])),
+                ([1,6] .+ total_vertices, minimum([vertices[1][2], vertices[6][2]])),
+                ([1,7] .+ total_vertices, minimum([vertices[1][2], vertices[7][2]])),
+                ([1,8] .+ total_vertices, minimum([vertices[1][2], vertices[8][2]])),
+                ([1,9] .+ total_vertices, minimum([vertices[1][2], vertices[9][2]])),
+                ([2,3] .+ total_vertices, minimum([vertices[2][2], vertices[3][2]])),
+                ([3,4] .+ total_vertices, minimum([vertices[3][2], vertices[4][2]])),
+                ([4,5] .+ total_vertices, minimum([vertices[4][2], vertices[5][2]])),
+                ([5,6] .+ total_vertices, minimum([vertices[5][2], vertices[6][2]])),
+                ([6,7] .+ total_vertices, minimum([vertices[6][2], vertices[7][2]])),
+                ([7,8] .+ total_vertices, minimum([vertices[7][2], vertices[8][2]])),
+                ([8,9] .+ total_vertices, minimum([vertices[8][2], vertices[9][2]])),
+                ([9,2] .+ total_vertices, minimum([vertices[9][2], vertices[2][2]]))
+            ]
+            triangles = [
+                ([1,2,3] .+ total_vertices, minimum([vertices[1][2], vertices[2][2], vertices[3][2]])),
+                ([1,3,4] .+ total_vertices, minimum([vertices[1][2], vertices[3][2], vertices[4][2]])),
+                ([1,4,5] .+ total_vertices, minimum([vertices[1][2], vertices[4][2], vertices[5][2]])),
+                ([1,5,6] .+ total_vertices, minimum([vertices[1][2], vertices[5][2], vertices[6][2]])),
+                ([1,6,7] .+ total_vertices, minimum([vertices[1][2], vertices[6][2], vertices[7][2]])),
+                ([1,7,8] .+ total_vertices, minimum([vertices[1][2], vertices[7][2], vertices[8][2]])),
+                ([1,8,9] .+ total_vertices, minimum([vertices[1][2], vertices[8][2], vertices[9][2]])),
+                ([1,9,2] .+ total_vertices, minimum([vertices[1][2], vertices[9][2], vertices[2][2]]))
+            ]
+            total_vertices += 9
+            filtration = [filtration; vertices; edges; triangles]
+        end
+        if length(part_one) != length(part_two)
+            if length(part_one) < length(part_two)
+                part_one, part_two = part_two, part_one
+            end
+            u,v,w = part_one[1], part_one[2], part_one[3]
+            x = part_two[1]
+
+            barycenters = [barycenters; [
+                get_barycenter(points, [u,v,w,x]),  #1
+                get_barycenter(points, [x,v]),      #2
+                get_barycenter(points, [x,v,w]),    #3
+                get_barycenter(points, [x,w]),      #4
+                get_barycenter(points, [w,u,x]),    #5
+                get_barycenter(points, [u,x]),      #6
+                get_barycenter(points, [u,v,x]),    #7
+            ]]
+            vertices = [
+                ([1] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [u,v,w])) / 2.0), 
+                ([2] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [v])) / 2.0),
+                ([3] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [v,w])) / 2.0),
+                ([4] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [w])) / 2.0),
+                ([5] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [w,u])) / 2.0),
+                ([6] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [u])) / 2.0),
+                ([7] .+ total_vertices, euclidean(get_barycenter(points, [x]), get_barycenter(points, [u,v])) / 2.0),
+            ]
+            edges = [
+                ([1,2] .+ total_vertices, minimum([vertices[1][2], vertices[2][2]])),
+                ([1,3] .+ total_vertices, minimum([vertices[1][2], vertices[3][2]])),
+                ([1,4] .+ total_vertices, minimum([vertices[1][2], vertices[4][2]])),
+                ([1,5] .+ total_vertices, minimum([vertices[1][2], vertices[5][2]])),
+                ([1,6] .+ total_vertices, minimum([vertices[1][2], vertices[6][2]])),
+                ([1,7] .+ total_vertices, minimum([vertices[1][2], vertices[7][2]])),
+                ([2,3] .+ total_vertices, minimum([vertices[2][2], vertices[3][2]])),
+                ([3,4] .+ total_vertices, minimum([vertices[3][2], vertices[4][2]])),
+                ([4,5] .+ total_vertices, minimum([vertices[4][2], vertices[5][2]])),
+                ([5,6] .+ total_vertices, minimum([vertices[5][2], vertices[6][2]])),
+                ([6,7] .+ total_vertices, minimum([vertices[6][2], vertices[7][2]])),
+                ([7,2] .+ total_vertices, minimum([vertices[7][2], vertices[2][2]]))
+            ]
+            triangles = [
+                ([1,2,3] .+ total_vertices, minimum([vertices[1][2], vertices[2][2], vertices[3][2]])),
+                ([1,3,4] .+ total_vertices, minimum([vertices[1][2], vertices[3][2], vertices[4][2]])),
+                ([1,4,5] .+ total_vertices, minimum([vertices[1][2], vertices[4][2], vertices[5][2]])),
+                ([1,5,6] .+ total_vertices, minimum([vertices[1][2], vertices[5][2], vertices[6][2]])),
+                ([1,6,7] .+ total_vertices, minimum([vertices[1][2], vertices[6][2], vertices[7][2]])),
+                ([1,7,2] .+ total_vertices, minimum([vertices[1][2], vertices[7][2], vertices[2][2]]))
+            ]
+            total_vertices += 7
+            filtration = [filtration; vertices; edges; triangles]
+        end
+    end
+    barycenters, filtration
+end
+
+function calculate_persistence_diagram(upper_star_filtration)
     py"""
-    import oineus as oin
     import numpy as np
-    import torch
-    import diode
-
-    def get_interface_diagram(points, n_atoms_per_mol):
-        points = np.asarray(points)
-        simplices = diode.fill_alpha_shapes(points)
-        fil = oin.Filtration_double([oin.Simplex_double(s[0], s[1]) for s in simplices])
-
-        def is_multi(sigma):
-            return len(set(v // n_atoms_per_mol for v in sigma.vertices)) >= 2
-        fil = fil.subfiltration(is_multi)
-
-        def recalculate_filtration_value(cell):
-            parts = [v // n_atoms_per_mol for v in cell.vertices]
-            n_parts = len(set(parts))
-            p_agg = [np.array([0.0, 0.0, 0.0]) for _ in range(n_parts)]
-            weights = [0 for _ in range(n_parts)]
-            for i, p in enumerate(parts):
-                p_agg[p] += points[cell.vertices[i]]
-                weights[p] += 1
-            bcs = [p_agg[i] / weights[i] for i in range(n_parts)]
-            filtration_value = sum([np.linalg.norm(bcs[i] - bcs[j]) for i in range(n_parts) for j in range(i + 1, n_parts)]) / n_parts
-            # convert to native Python float from numpy
-            return float(filtration_value)
-
-        new_values = [recalculate_filtration_value(cell) for cell in fil]
-        fil.set_values(new_values)
-
+    import oineus as oin 
+    def calculate_persistence_diagram(upper_star_filtration):
+        fil = oin.Filtration_double([oin.Simplex_double([v-1 for v in s[0]], s[1]) for s in upper_star_filtration], True)
         dcmp = oin.Decomposition(fil, True)
         params = oin.ReductionParams()
         params.clearing_opt = False
@@ -116,5 +181,13 @@ function get_interface_diagram(points, n_atoms_per_mol)
         dgm = dcmp.diagram(fil, include_inf_points=False)
         return dgm
     """
-    py"get_interface_diagram"(points, n_atoms_per_mol)
+    py"calculate_persistence_diagram"(upper_star_filtration)
+end
+
+function get_interface_with_persistence(points, n_atoms_per_mol)
+    mc_tets = get_multichromatic_tetrahedra(points, n_atoms_per_mol)
+    barycenters, filtration = get_barycentric_subdivision_and_filtration(points, mc_tets)
+    dgms = calculate_persistence_diagram(filtration)
+    dgms = [dgms[i] for i in 1:2]
+    barycenters, filtration, dgms
 end
