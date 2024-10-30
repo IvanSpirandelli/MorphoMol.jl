e0c78d2082f1d6715d416fb4b698fad45e9bb599
Ivan Spirandelli <ivan@spirandelli.de>
2024-10-30 14:08:04 -0700

    exporting interface functions

--- src/modules/Energies/src/Energies.jl
+++ src/modules/Energies/src/Energies.jl
@@ -3,7 +3,7 @@ module Energies
     export get_geometric_measures, get_geometric_measures_with_derivatives
     export get_geometric_measures_and_overlap_value, get_geometric_measures_and_overlap_value_with_derivatives
     export get_total_persistence_summed, get_total_persistence, get_death_by_birth_persistence, get_death_by_birth_persistence_summed
-    export get_interface_diagram, get_interface_diagram_and_geometry
+    export get_multichromatic_tetrahedra, get_barycentric_subdivision_and_filtration, get_interface_diagram, get_interface_diagram_and_geometry
     export get_persistence_diagram
 
     using PyCall
--- src/modules/Utilities/src/load_jld2.jl
+++ src/modules/Utilities/src/load_jld2.jl
@@ -1,24 +1,25 @@
-function load_jld2(file)
-    mutable struct SimulationOutput
-        states::Vector{Vector{Float64}}
-        Es::Vector{Float64}
-        measures::Vector{Vector{Float64}}
-        αs::Vector{Float32}
-    end
+#TODO: Old output formats. Might need to be updated to the new format.
+# function load_jld2(file)
+#     mutable struct SimulationOutput
+#         states::Vector{Vector{Float64}}
+#         Es::Vector{Float64}
+#         measures::Vector{Vector{Float64}}
+#         αs::Vector{Float32}
+#     end
 
-    mutable struct SimulationStates
-        states::Vector{Vector{Float64}}
-        αs::Vector{Float32}
-    end
+#     mutable struct SimulationStates
+#         states::Vector{Vector{Float64}}
+#         αs::Vector{Float32}
+#     end
 
-    mutable struct MorphometricSimulationOutput
-        states::Vector{Vector{Float64}}
-        Es::Vector{Float64}
-        Vs::Vector{Float32}
-        As::Vector{Float32}
-        Ms::Vector{Float32}
-        Xs::Vector{Float32}
-        OLs::Vector{Float32}
-        αs::Vector{Float32}
-    end
-end
+#     mutable struct MorphometricSimulationOutput
+#         states::Vector{Vector{Float64}}
+#         Es::Vector{Float64}
+#         Vs::Vector{Float32}
+#         As::Vector{Float32}
+#         Ms::Vector{Float32}
+#         Xs::Vector{Float32}
+#         OLs::Vector{Float32}
+#         αs::Vector{Float32}
+#     end
+# end
