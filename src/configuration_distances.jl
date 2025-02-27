7cc12bf8d9fe4577e33dc6654f8f4aafb78f71e1
Ivan Spirandelli <ivan@spirandelli.de>
2025-02-26 16:33:05 -0800

    implementing cc_sa

--- src/configuration_distances.jl
+++ src/configuration_distances.jl
@@ -1,3 +1,32 @@
+using Combinatorics
+
+# General case:
+function get_matched_distances_between_transformation_offsets(template_centers::Matrix{Float64}, state_a::Vector{Float64}, state_b::Vector{Float64}, subset_a::Vector{Int}, subset_b::Vector{Int})
+    function transform_dist(template_centers, state, ss, idx)
+        i = ss[1]
+        j = ss[2]
+        R1 = exp(RotationVecGenerator(state[(i-1) * 6 + 1:(i-1) * 6 + 3]...))
+        T1 = state[(i-1) * 6 + 4:(i-1) * 6 + 6]
+        R2 = exp(RotationVecGenerator(state[(j-1) * 6 + 1:(j-1) * 6 + 3]...))
+        T2 = state[(j-1) * 6 + 4:(j-1) * 6 + 6]
+        euclidean(T1 + R1 * template_centers[:,idx], T2 + R2 * template_centers[:,idx])
+    end
+    [abs(transform_dist(template_centers, state_a, subset_a, i) - transform_dist(template_centers, state_b, subset_b, i)) for i in 1:size(template_centers)[2]]
+end
+
+function sum_of_permutation(template_centers::Matrix{Float64}, state_a::Vector{Float64}, state_b::Vector{Float64}, perm_a::Vector{Int}, perm_b::Vector{Int}, all_pairs::Vector{Vector{Int}}, n_mol::Int)
+    n = size(template_centers)[2]
+    sum(sum(get_matched_distances_between_transformation_offsets(template_centers, state_a, state_b, perm_a[p], perm_b[p]))/ n for p in all_pairs) / n_mol
+end
+
+function average_offset_distance(template_centers::Matrix{Float64}, state_a::Vector{Float64}, state_b::Vector{Float64}, n_mol::Int)
+    perms = collect(permutations(1:n_mol))
+    def_perm = collect(1:n_mol)
+    pairings = collect(combinations(def_perm, 2))
+    minimum([sum_of_permutation(template_centers, state_a, state_b, perms[1], def_perm, pairings, n_mol) for perm in collect(permutations(def_perm))])
+end
+
+# Specfic cases for two and three molecules where template centers are also oriented differently.
 function get_matched_distances_between_transformation_offsets(template_centers_a::Matrix{Float64}, template_centers_b::Matrix{Float64}, state_a::Vector{Float64}, state_b::Vector{Float64})
     function transform_dist(template_centers, state, idx)
         i = 1
--- src/modules/Algorithms/src/simulated_annealing.jl
+++ src/modules/Algorithms/src/simulated_annealing.jl
@@ -65,3 +65,48 @@ function simulate!(algorithm::SimulatedAnnealing, x::Vector{Float64}, simulation
 
     return output
 end
+
+struct ConnectedComponentSimulatedAnnealing{E, P, TR, ICC}
+    energy::E
+    perturbation::P
+    temperature_reduction::TR
+    get_initial_connected_components::ICC
+end
+
+function simulate!(algorithm::ConnectedComponentSimulatedAnnealing, x::Vector{Float64}, simulation_time_minutes::Float64, output::Dict{String, Vector})
+    start_time = now()
+    energy = algorithm.energy
+    perturbation = algorithm.perturbation
+    get_initial_connected_components = algorithm.get_initial_connected_components
+    temperature_reduction = algorithm.temperature_reduction
+    
+    T = temperature_reduction(0.0)
+
+    x_cand = deepcopy(x)
+    icc = deepcopy(get_initial_connected_components(x))
+    E, measures, _ = energy(icc, 1, x)
+
+    accepted_steps = 0
+    total_steps = 0
+
+    add_to_output(merge!(measures, Dict("Es" => E, "states" => x, "αs" => 0.0)), output)
+
+    time_passed = Dates.value(now() - start_time) / 60000.0
+    while time_passed < simulation_time_minutes
+        total_steps += 1
+        i, x_cand = perturbation(x)
+        E_cand, measures, ucc = energy(icc, i, x_cand)
+
+        if rand() < exp(-(1.0/T)*(E_cand - E))
+            E = E_cand
+            accepted_steps += 1
+            x = deepcopy(x_cand)
+            icc = deepcopy(ucc)
+            add_to_output(merge!(measures,Dict("Es" => E, "states" => x, "αs" => accepted_steps/total_steps)), output)
+        end
+        time_passed = Dates.value(now() - start_time) / 60000.0
+        T = temperature_reduction(time_passed)
+    end
+
+    return output
+end
