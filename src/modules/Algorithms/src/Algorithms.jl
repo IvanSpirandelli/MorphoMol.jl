209b25148d8b9443a8450890f90a85928f116e9b
Ivan Spirandelli <ivan@spirandelli.de>
2025-03-10 09:53:16 +0100

    more declarations

--- src/modules/Algorithms/src/Algorithms.jl
+++ src/modules/Algorithms/src/Algorithms.jl
@@ -9,6 +9,12 @@ module Algorithms
         end
     end
 
+    function add_to_output(measures::Dict{String, Float64}, output::Dict{String, Vector})
+        for (k, v) in measures
+            push!(output[k], v)
+        end
+    end
+
     include("hamiltonian_monte_carlo.jl")
     include("connected_component_random_walk_metropolis.jl")
     include("random_walk_metropolis.jl")
